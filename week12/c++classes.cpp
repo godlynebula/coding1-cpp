f2e195d9bea52b0d70b0613e482bd0d95113eedb
godlynebula <anokiadams@yahoo.com>
2025-04-15 10:20:51 -0500

    Mid-Class Tweaks
    fixes and tweaks to the code, during class.

--- week12/c++classes.cpp
+++ week12/c++classes.cpp
@@ -18,25 +18,41 @@ int GetDamage()
 #include <string>
 using namespace std;
 
+// global debugging variable
+bool debug = true;
+
 class Human {
     string name;
     int health;
-    int damage;
+    int damage;         // no values here, causes warnings on Mac compiler
 
 public:
 
-Human(string givenName, int givenHealth = 10, int baseDamage = 4) {
+Human(string givenName, int givenHealth = 10, int baseDamage = 4) {     // defaults instead go in the constructor
     name = givenName;
     health = givenHealth;
     damage = baseDamage;
+
+    SayHello();
+}
+
+Human() {
+    SayHello();
 }
 
 void SayHello() {
     cout << "Hey, how's it going? My name is " << name << ".\n";
+    cout << "My health is: " << health << " and my damage is: " << damage << ".\n";
 }
     
     void SetHealth(int byAmount) {
-            health = byAmount;
+            if (debug) {
+                cout << "at top of SetHealth(byAmount), health = " << health << ", byAmount " << byAmount << ".\n"; 
+            }
+             health += byAmount;
+
+            if (health < 0) health = 0;         // I had this originally, and deleted it in class. Back it comes. Never doubt myself.
+            if (health > 100) health = 100;     // also, you can one-line if statements and such if they only use one line.     
     }
 
     void SetDamage(int givenDamage) {
@@ -54,9 +70,6 @@ void SayHello() {
 
 int main() {                        // not in the assignment, just thought I would check on the code
     Human anoki("Anoki", 10, 4);
-    anoki.SayHello();
-    cout << "My health is: " << anoki.GetHealth() << " and my damage is: " << anoki.GetDamage() << ".\n";
-
     anoki.SetHealth(25);
     anoki.SetDamage(7);
 
